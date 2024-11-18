cf5e8f217407a80aa7ec1dd78a0ff3d53efe4fb2
AJL-1998 <73225992+AJL-1998@users.noreply.github.com>
2024-11-18 12:20:35 +0000

    Inducing Point Model

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
-import gpytorch
+import gpytorch, torch
+from gpytorch.lazy import AddedDiagLazyTensor, RootLazyTensor, NonLazyTensor, ConstantDiagLazyTensor
 
 class ExactGPModel(gpytorch.models.ExactGP):
 
@@ -28,10 +29,55 @@ class ExactGPModel(gpytorch.models.ExactGP):
                 gpytorch.kernels.keops.MaternKernel(nu=2.5, ard_num_dims=train_x.size(-1))
             )
         else:
-            assert 0
+            raise NotImplementedError("Please choose a valid kernel function")
 
     def forward(self, x):
         mean_x = self.mean_module(x)
         covar_x = self.covar_module(x)
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
-    
+
+class InducingGPModel(gpytorch.models.ExactGP):
+
+    def __init__(self, train_x, train_y, likelihood, inducing_points, kernel="RBF-ARD"):
+        super(InducingGPModel, self).__init__(train_x, train_y, likelihood)
+
+        self.mean_module = gpytorch.means.ConstantMean()
+
+        if kernel == "RBF-ARD":
+            print("Initialising with RBF-ARD Kernel")
+            self.covar_module = gpytorch.kernels.ScaleKernel(
+                gpytorch.kernels.keops.RBFKernel(ard_num_dims=train_x.size(-1))
+            )
+        elif kernel == "RBF":
+            print("Initialising with RBF Kernel")
+            self.covar_module = gpytorch.kernels.ScaleKernel(
+                gpytorch.kernels.keops.RBFKernel()
+            )
+        elif kernel == "Matern32":
+            print("Initialising with Matern 1.5 Kernel")
+            self.covar_module = gpytorch.kernels.ScaleKernel(
+                gpytorch.kernels.keops.MaternKernel(nu=1.5, ard_num_dims=train_x.size(-1))
+            )
+        elif kernel == "Matern52":
+            print("Initialising with Matern 2.5 Kernel")
+            self.covar_module = gpytorch.kernels.ScaleKernel(
+                gpytorch.kernels.keops.MaternKernel(nu=2.5, ard_num_dims=train_x.size(-1))
+            )
+        else:
+            raise NotImplementedError("Please choose a valid kernel function")
+        
+        self.inducing_points = torch.nn.Parameter(
+            inducing_points
+        )
+        
+    def forward(self, x):
+
+        mean_x = self.mean_module(x)
+
+        tall = self.covar_module(x, self.inducing_points)
+        small = self.covar_module(self.inducing_points, self.inducing_points)
+        L = gpytorch.utils.cholesky.psd_safe_cholesky(small.evaluate())
+        P = torch.linalg.solve_triangular(L, tall.t().evaluate())
+
+        return gpytorch.distributions.MultivariateNormal(mean_x, RootLazyTensor(P.T))
+
